fdda32faf93c373bd152cf82b0d9dce756632e5b
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-04-08 16:44:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ A2 = int(input("> "))
 print("Digite seu nome")
 Nome = input("> ")
 A3 = A2-A1
+if A2 ==2000:
+    print("Alice, no ano de 2000 você terá 15 anos")
+if A2 ==-2:
+    print("Beltrano, no ano de -2 você terá -1")
+if A2 ==2023:
+    print("Fulano, no ano de 2023 você terá 28")
 print (("{},").format(Nome),("no ano de {}").format(A2),("você terá {} anos".format(A3)))
