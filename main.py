11d11554a557a9b337fc7fd7a0f225fe23aa40b2
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-04-08 16:33:41 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Digite o ano que nasceu")
+A1 = int(input("> "))
+print("Digite sua idade atual")
+IdadeAtual = int(input("> "))
+print("Digite o ano que você que testa")
+A2 = int(input("> "))
+print("Digite seu nome")
+Nome = input("> ")
+A3 = A2-A1
+print (("{},").format(Nome),("no ano de {}").format(A2),("você terá {} anos".format(A3)))
