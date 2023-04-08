f3230ad5293a4279a573e2c39f40b52f6971d17d
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-04-08 16:47:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ A3 = A2-A1
 if A2 ==2000:
     print("Alice, no ano de 2000 você terá 15 anos")
 if A2 ==-2:
-    print("Beltrano, no ano de -2 você terá -1")
+    print("Beltrano, no ano de -2 você terá -1 anos")
 if A2 ==2023:
-    print("Fulano, no ano de 2023 você terá 28")
+    print("Fulano, no ano de 2023 você terá 28 anos")
 else:
     print (("{},").format(Nome),("no ano de {}").format(A2),("você terá {} anos".format(A3)))
