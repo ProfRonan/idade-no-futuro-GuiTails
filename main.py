33680e699d7cc193056246556496e4eb01cafe5e
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-05-24 10:56:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ print("Digite o ano que você que testa")
 A2 = int(input("> "))
 print("Digite seu nome")
 Nome = input("> ")
-A3 = A2-A1
-if A2 ==2000:
-    print("Alice, no ano de 2000 você terá 15 anos")
-if A2 ==-2:
-    print("Beltrano, no ano de -2 você terá -1 anos")
-if A2 ==2023:
-    print("Fulano, no ano de 2023 você terá 28 anos")
-else:
-    print (("{},").format(Nome),("no ano de {}").format(A2),("você terá {} anos".format(A3)))
+A3 = A2-A1+IdadeAtual
+
+print (("{},").format(Nome),("no ano de {}").format(A2),("você terá {} anos".format(A3)))
